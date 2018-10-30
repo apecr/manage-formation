1ae26367b833daec3c9a5ad02f397a3263f4371b
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-10-30 14:34:57 +0100

    chore() coverage sonar

--- test/db.test.js
+++ test/db.test.js
@@ -7,8 +7,6 @@ const {getElements} = require('../src/db');
 describe('Get the elements', () => {
   it('Should get all the elements in the db', async() => {
     const elements = await getElements();
-    console.log(JSON.stringify(elements[0], undefined, 2));
-    console.log(elements.reduce((acc, el) => acc + el.price, 0));
     expect(elements).to.be.an('Array').and.have.length(8);
   });
 });
